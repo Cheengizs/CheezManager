e1f48f9ffc76d33722fcbc5884f748496269ae9b
Cheengizs <valinskiyartem@gmail.com>
2025-09-26 18:07:00 +0300

    created domain models

---
+++ Domain/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models;
+
+public class Category
+{
+    public Guid Id { get; private set; }
+    public string ShortName { get; private set; }
+    public string Description { get; private set; }
+    public User User { get; private set; }
+}
---
+++ Domain/Models/Goal.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Models;
+
+public class Goal
+{
+    public Guid Id { get; private set; }
+    public string ShortName { get; private set; }
+    public string Description { get; private set; }
+    public DateTime CreatedDate { get; private set; }
+    public DateTime EstimatedEndDate { get; private set; }
+    public DateTime EndDate { get; private set; }
+    public int Amount { get; private set; }
+    public bool IsCompleted { get; private set; }
+    public Category Category { get; private set; }
+    public User User { get; private set; }
+    
+    public void Complete()
+    {
+        IsCompleted = true;
+        EndDate = DateTime.UtcNow;
+    }
+}
